88b268de0502cc24e8c872aa63417341af4f02af
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2025-08-16 16:25:26 +0200

    Remove struct_viewer and btparser from cross

--- src/cross/vendor/btparser/btparser/preprocessor.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <string>
-#include <unordered_map>
-
-std::string preprocess(const std::string& input, std::string& error, const std::unordered_map<std::string, std::string>& definitions);
