099a17a16a15a8c2a76198af6a01d55076049795
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2025-08-19 19:52:09 +0200

    Build x64dbg.chm in CI
    Closes #3687

--- docs/makechm.bat
+++ docs/makechm.bat
@@ -1,13 +1,32 @@
 @echo off
-if "%VSVARSALLPATH%"=="" set VSVARSALLPATH=c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat
-echo Setting VS Path
-call "%VSVARSALLPATH%"
+
+cd /d "%~dp0"
+
+set PORTABLE_PYTHON=%~dp0python-2.7.18.amd64.portable
+if not exist "%PORTABLE_PYTHON%\python.exe" (
+	echo Downloading portable Python...
+	curl.exe -L -O https://github.com/x64dbg/docs/releases/download/python27-portable/python-2.7.18.amd64.portable.7z
+	7z x python-2.7.18.amd64.portable.7z -opython-2.7.18.amd64.portable
+)
+
+if not exist "%~dp0hhc.exe" (
+	echo Downloading HTML Help Workshop...
+	pause
+	curl.exe -L -O https://github.com/x64dbg/deps/releases/download/dependencies/hhc-4.74.8702.7z
+	7z x hhc-4.74.8702.7z
+)
+
 echo Building Help Project
 call make htmlhelp
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 echo Applying CHM hacks
 copy theme.js .\_build\htmlhelp\_static\js\theme.js
 type hacks.css >> .\_build\htmlhelp\_static\css\theme.css
 echo Building CHM File
-hhc .\_build\htmlhelp\x64dbgdoc.hhp
-copy .\_build\htmlhelp\x64dbgdoc.chm x64dbg.chm
+hhc.exe .\_build\htmlhelp\x64dbgdoc.hhp
+copy /Y .\_build\htmlhelp\x64dbgdoc.chm x64dbg.chm
+if not exist "x64dbg.chm" (
+	echo Failed to build CHM file!
+	exit /b 1
+)
 echo Finished
